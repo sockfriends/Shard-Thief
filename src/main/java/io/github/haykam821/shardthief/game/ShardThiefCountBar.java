18d52012e674d4614c0d0ab055084a80b0eff0e6
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-03-07 12:51:04 -0500

    Change the boss bar title color to aqua

--- src/main/java/io/github/haykam821/shardthief/game/ShardThiefCountBar.java
+++ src/main/java/io/github/haykam821/shardthief/game/ShardThiefCountBar.java
@@ -3,14 +3,17 @@ package io.github.haykam821.shardthief.game;
 import io.github.haykam821.shardthief.game.phase.ShardThiefActivePhase;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import xyz.nucleoid.plasmid.widget.BossBarWidget;
 import xyz.nucleoid.plasmid.widget.GlobalWidgets;
 
 public class ShardThiefCountBar {
+	private static final Formatting TITLE_COLOR = Formatting.AQUA;
+
 	private final BossBarWidget bar;
 
 	public ShardThiefCountBar(Text title, GlobalWidgets widgets) {
-		this.bar = widgets.addBossBar(title, BossBar.Color.BLUE, BossBar.Style.PROGRESS);
+		this.bar = widgets.addBossBar(title.shallowCopy().formatted(TITLE_COLOR), BossBar.Color.BLUE, BossBar.Style.PROGRESS);
 	}
 
 	public void tick(ShardThiefActivePhase phase) {
